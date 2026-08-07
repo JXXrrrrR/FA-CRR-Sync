4bc93d70dadd863f0847c80a70734a75bb8aa8ad
JXXrrrrR <113779131+JXXrrrrR@users.noreply.github.com>
2026-08-07 21:00:13 +0300

    Delete src/fa_crr_sync directory

--- src/fa_crr_sync/__init__.py
+++
@@ -1,3 +0,0 @@
-"""FA-CRR-Sync maintained implementation package."""
-
-__version__ = "0.1.0"
--- src/fa_crr_sync/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Shared implementation utilities."""
-
-from .reproducibility import seed_everything
-
-__all__ = ["seed_everything"]
